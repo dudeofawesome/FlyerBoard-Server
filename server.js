f7641f1e493d7a0ce792f7f7ba329411ee32c4be
Louis Orleans <louis@0rleans.com>
2015-03-29 15:12:22 -0700

    flyers work now

--- server.js
+++ server.js
@@ -27,226 +27,223 @@ var failedAuthAttempts = [];
 var disabledSocketIDs = [];
 var connectedSockets = [];
 
-io.on('connection', function (socket) {
-	console.log("new user connected");
-	connectedSockets[socket.id] = true;
-	socket.on('get fliers', function(msg) {
-		console.log("get fliers");
-		db.items.find(function(err, items) {
-			if( err || !items || items.length == 0) io.to(socket.id).emit('get fliers', "No fliers were found.");
-			else {
-				io.to(socket.id).emit('get fliers', items);
-			}
-		});
-	});
-	socket.on('get flier', function(msg) {
-		console.log("get flier");
-		db.items.find({_id: msg._id}, function(err, item) {
-			if( err || !item || item.length == 0) io.to(socket.id).emit('get flier', "Bad _id.");
-			else {
-				io.to(socket.id).emit('get flier', item);
-			}
-		});
-	});
-	socket.on('login', function(msg){
-		console.log("user trying to login");
-		stopBruteForce(msg.username);
-		if (disabledSocketIDs[msg.username] != null) {
-			if (DATE.getTime() < disabledSocketIDs[msg.username]) {
-				console.log("A user tried to log in too many times");
-				io.to(socket.id).emit('login', "You've failed too many times.");
-				return;
-			}
-			else {
-				disabledSocketIDs[msg.username] = null;
-			}
+// connectedSockets[socket.id] = true;
+app.get("/flyers", function (req, res) {
+	console.log("get flyers");
+	db.fliers.find(function(err, items) {
+		if( err || !items || items.length == 0) res.send("No flyers were found.");
+		else {
+			res.send("flyers->" + items[0].flyers);
 		}
-		db.users.find({username: msg.username}, function(err, users) {
-			if( err || !users || users.length == 0) io.to(socket.id).emit('login', "failed to authenticate login");
-			else {
-				if (comparePassword(users[0].password, msg.password)) {
-					var sesh = {}
-					sesh.id = users[0]._id;
-					sesh.authKey = determineAuthKey(sesh.id);
-					sesh.username = msg.username;
-					sesh.socketID = socket.id;
-					userSessions[sesh.id] = sesh;
-					failedAuthAttempts[msg.username] = null;
-					console.log(sesh.username + " is logging in.");
-					io.to(socket.id).emit('login', {authKey: sesh.authKey, id: sesh.id, username: sesh.username});
-				} else {
-					io.to(socket.id).emit('login', "failed to authenticate login");
-				}
-			}
-		});
-	});
-	socket.on('logout', function(msg){
-		// TODO clear GCM and APNS data
-		console.log("logout");
-		if (userSessions[msg.ID] != null && userSessions[msg.ID].authKey == msg.authKey)
-				userSessions[msg.ID] = null;
-	});
-	socket.on('create account', function(msg){
-		console.log("create account");
-		db.users.find({username: msg.username}, function(err, users) {
-			//verify there are no other users with username and that the email is valid
-			if (users.length == 0 && validateEmail(msg.email)) {
-				db.users.save({username: msg.username, password: hashPassword(msg.password), email: msg.email}, function(err, saved) {
-					if( err || !saved ) console.log("User not created");
-					else {
-						var sesh = {}
-						sesh.authKey = determineAuthKey(saved._id);
-						sesh.id = saved._id;
-						sesh.username = saved.username;
-						userSessions[sesh.id] = sesh;
-						io.to(socket.id).emit('login', {authKey: sesh.authKey, id: sesh.id, username: sesh.username});
-						console.log("User created");
-					}
-				});
-			} else {
-				if (users.length != 0)
-					io.to(socket.id).emit('create account', "username already taken");
-				else if (!validateEmail(msg.email))
-					io.to(socket.id).emit('create account', "invalid email");
-			}
-		});
-	});
-	socket.on('change password', function(msg){
-		console.log("change password");
-		stopBruteForce(msg.ID);
-		if (disabledSocketIDs[msg.ID] != null) {
-			if (DATE.getTime() < disabledSocketIDs[msg.ID]) {
-				io.to(socket.id).emit('change password', "Failed to authenticate password change. You've been locked out.");
-				return;
-			}
-			else {
-				disabledSocketIDs[msg.ID] = null;
-			}
-		}
-		if (userSessions[msg.ID] != null && userSessions[msg.ID].authKey == msg.authKey) {
-			db.users.find({_id: db.ObjectId(msg.ID)}, function(err, users) {
-				if (users[0].username == msg.username && comparePassword(users[0].password, msg.password) && validatePassword(msg.newPassword) == true) {
-					db.users.update({_id: db.ObjectId(msg.ID)}, {$set: {password: hashPassword(msg.newPassword)}});
-					io.emit('change password', "Successfully changed password.");
-				} else {
-					var validateResponse = validatePassword(msg.newPassword);
-					if (validateResponse != true)
-						io.to(socket.id).emit('change password', validateResponse);
-					else
-						io.to(socket.id).emit('change password', "The old password was incorrect.");
-				}
-				failedAuthAttempts[msg.ID] = null;
-			});
-		} else {
-			io.to(socket.id).emit('change password', "Failed to authenticate password change.");
-		}
-	});
-	socket.on('set GCMregID', function(msg){
-		console.log("set GCM regID");
-		stopBruteForce(msg.ID);
-		if (disabledSocketIDs[msg.ID] != null) {
-			if (DATE.getTime() < disabledSocketIDs[msg.ID]) {
-				io.to(socket.id).emit('set GCMregID', "Failed to authenticate GCM regID change. You've been locked out.");
-				return;
-			}
-			else {
-				disabledSocketIDs[msg.ID] = null;
-			}
-		}
-		if (userSessions[msg.ID] != null && userSessions[msg.ID].authKey == msg.authKey) {
-			db.users.find({_id: db.ObjectId(msg.ID)}, function(err, users) {
-				db.users.update({_id: db.ObjectId(msg.ID)}, {$set: {GCMregID: msg.GCMregID}});
-				io.emit('set GCMregID', "Successfully changed GCM regID.");
-				failedAuthAttempts[msg.ID] = null;
-			});
-		} else {
-			io.to(socket.id).emit('set GCMregID', "Failed to authenticate GCM regID change.");
-		}
-	});
-	socket.on('set APNSregID', function(msg){
-		console.log("set APNS regID");
-		stopBruteForce(msg.ID);
-		if (disabledSocketIDs[msg.ID] != null) {
-			if (DATE.getTime() < disabledSocketIDs[msg.ID]) {
-				io.to(socket.id).emit('set APNSregID', "Failed to authenticate APNS regID change. You've been locked out.");
-				return;
-			}
-			else {
-				disabledSocketIDs[msg.ID] = null;
-			}
-		}
-		if (userSessions[msg.ID] != null && userSessions[msg.ID].authKey == msg.authKey) {
-			db.users.find({_id: db.ObjectId(msg.ID)}, function(err, users) {
-				db.users.update({_id: db.ObjectId(msg.ID)}, {$set: {APNSregID: msg.APNSregID}});
-				io.emit('set APNSregID', "Successfully changed APNS regID.");
-				failedAuthAttempts[msg.ID] = null;
-			});
-		} else {
-			io.to(socket.id).emit('set APNSregID', "Failed to authenticate APNS regID change.");
-		}
-	});
-	socket.on('new flyer', function(msg){
-		// TODO check to ensure all values are valid (including that they are the correct type)
-		console.log("new flyer");
-		stopBruteForce(msg.bidder)
-		if (disabledSocketIDs[msg.bidder] != null) {
-			if (DATE.getTime() < disabledSocketIDs[msg.bidder]) {
-				io.to(socket.id).emit('new flyer', "You've failed too many times.");
-				return;
-			}
-			else {
-				disabledSocketIDs[msg.bidder] = null;
-			}
-		}
-		// verify user session
-		if (userSessions[msg.bidder] != null && userSessions[msg.bidder].authKey == msg.authKey){
-			// this still needs lots of work!
-			db.items.find({_id: db.ObjectId(msg._id)}, function(err, items) {
-				// send flyer notification
-				if (items[0].bidHistory[items[0].bidHistory.length - 1].bidder != msg.bidder) {
-					// TODO check the othersocket
-					if (userSessions[items[0].bidHistory[items[0].bidHistory.length - 1].bidder] != null && connectedSockets[userSessions[items[0].bidHistory[items[0].bidHistory.length - 1].bidder].socketID] == true) {
-						console.log("sending a flyer notification");
-						io.to(userSessions[items[0].bidHistory[items[0].bidHistory.length - 1].bidder].socketID).emit('outbid notification', {item: msg._id, price: msg.bid, bidder: msg.bidder});
-					} else {
-						// TODO send a push notification to the user's phone or an email or text
-						db.users.find({_id: db.ObjectId(items[0].bidHistory[items[0].bidHistory.length - 1].bidder)}, function(err, users) {
-							var GCMregID = "";
-							if( err || !users) console.log(err);
-							else GCMregID = users[0].GCMregID;
-							var message = {
-								registration_id: GCMregID, // required
-								collapse_key: "New flyers",
-								data: {
-									item: msg._id,
-									price: msg.bid,
-									bidder: msg.bidder
-								}
-							};
-							gcm.send(message, function(err, messageId){
-								if (err) {
-									console.log("Failed to send GCM");
-								} else {
-									console.log("Sent GCM with ID ", messageId);
-								}
-							});
-						});
-						db.users.find({_id: db.ObjectId(items[0].bidHistory[items[0].bidHistory.length - 1].bidder)}, function(err, users) {
-							var APNSregID = "";
-							if( err || !users) console.log(err);
-							else APNSregID = users[0].APNSregID;
-							// TODO send an Apple push notification
-						});
-					}
-				}
-				db.items.update({_id: db.ObjectId(msg._id)}, {$push: {bidHistory: {bid: msg.bid, bidder: msg.bidder, time: DATE.getHours() + ":" + DATE.getMinutes()}}});
-				io.emit('new flyer', {itemID: msg.itemID, _id: msg._id, bid: msg.bid, bidder: msg.bidder, time: DATE.getHours() + ":" + DATE.getMinutes()});
-				failedAuthAttempts[msg.bidder] = null;
-			});
-		} else
-			io.to(socket.id).emit('new flyer', "failed to authenticate flyer");
 	});
 });
+// socket.on('get flier', function(msg) {
+// 	console.log("get flier");
+// 	db.items.find({_id: msg._id}, function(err, item) {
+// 		if( err || !item || item.length == 0) io.to(socket.id).emit('get flier', "Bad _id.");
+// 		else {
+// 			io.to(socket.id).emit('get flier', item);
+// 		}
+// 	});
+// });
+// socket.on('login', function(msg){
+// 	console.log("user trying to login");
+// 	stopBruteForce(msg.username);
+// 	if (disabledSocketIDs[msg.username] != null) {
+// 		if (DATE.getTime() < disabledSocketIDs[msg.username]) {
+// 			console.log("A user tried to log in too many times");
+// 			io.to(socket.id).emit('login', "You've failed too many times.");
+// 			return;
+// 		}
+// 		else {
+// 			disabledSocketIDs[msg.username] = null;
+// 		}
+// 	}
+// 	db.users.find({username: msg.username}, function(err, users) {
+// 		if( err || !users || users.length == 0) io.to(socket.id).emit('login', "failed to authenticate login");
+// 		else {
+// 			if (comparePassword(users[0].password, msg.password)) {
+// 				var sesh = {}
+// 				sesh.id = users[0]._id;
+// 				sesh.authKey = determineAuthKey(sesh.id);
+// 				sesh.username = msg.username;
+// 				sesh.socketID = socket.id;
+// 				userSessions[sesh.id] = sesh;
+// 				failedAuthAttempts[msg.username] = null;
+// 				console.log(sesh.username + " is logging in.");
+// 				io.to(socket.id).emit('login', {authKey: sesh.authKey, id: sesh.id, username: sesh.username});
+// 			} else {
+// 				io.to(socket.id).emit('login', "failed to authenticate login");
+// 			}
+// 		}
+// 	});
+// });
+// socket.on('logout', function(msg){
+// 	// TODO clear GCM and APNS data
+// 	console.log("logout");
+// 	if (userSessions[msg.ID] != null && userSessions[msg.ID].authKey == msg.authKey)
+// 			userSessions[msg.ID] = null;
+// });
+// socket.on('create account', function(msg){
+// 	console.log("create account");
+// 	db.users.find({username: msg.username}, function(err, users) {
+// 		//verify there are no other users with username and that the email is valid
+// 		if (users.length == 0 && validateEmail(msg.email)) {
+// 			db.users.save({username: msg.username, password: hashPassword(msg.password), email: msg.email}, function(err, saved) {
+// 				if( err || !saved ) console.log("User not created");
+// 				else {
+// 					var sesh = {}
+// 					sesh.authKey = determineAuthKey(saved._id);
+// 					sesh.id = saved._id;
+// 					sesh.username = saved.username;
+// 					userSessions[sesh.id] = sesh;
+// 					io.to(socket.id).emit('login', {authKey: sesh.authKey, id: sesh.id, username: sesh.username});
+// 					console.log("User created");
+// 				}
+// 			});
+// 		} else {
+// 			if (users.length != 0)
+// 				io.to(socket.id).emit('create account', "username already taken");
+// 			else if (!validateEmail(msg.email))
+// 				io.to(socket.id).emit('create account', "invalid email");
+// 		}
+// 	});
+// });
+// socket.on('change password', function(msg){
+// 	console.log("change password");
+// 	stopBruteForce(msg.ID);
+// 	if (disabledSocketIDs[msg.ID] != null) {
+// 		if (DATE.getTime() < disabledSocketIDs[msg.ID]) {
+// 			io.to(socket.id).emit('change password', "Failed to authenticate password change. You've been locked out.");
+// 			return;
+// 		}
+// 		else {
+// 			disabledSocketIDs[msg.ID] = null;
+// 		}
+// 	}
+// 	if (userSessions[msg.ID] != null && userSessions[msg.ID].authKey == msg.authKey) {
+// 		db.users.find({_id: db.ObjectId(msg.ID)}, function(err, users) {
+// 			if (users[0].username == msg.username && comparePassword(users[0].password, msg.password) && validatePassword(msg.newPassword) == true) {
+// 				db.users.update({_id: db.ObjectId(msg.ID)}, {$set: {password: hashPassword(msg.newPassword)}});
+// 				io.emit('change password', "Successfully changed password.");
+// 			} else {
+// 				var validateResponse = validatePassword(msg.newPassword);
+// 				if (validateResponse != true)
+// 					io.to(socket.id).emit('change password', validateResponse);
+// 				else
+// 					io.to(socket.id).emit('change password', "The old password was incorrect.");
+// 			}
+// 			failedAuthAttempts[msg.ID] = null;
+// 		});
+// 	} else {
+// 		io.to(socket.id).emit('change password', "Failed to authenticate password change.");
+// 	}
+// });
+// socket.on('set GCMregID', function(msg){
+// 	console.log("set GCM regID");
+// 	stopBruteForce(msg.ID);
+// 	if (disabledSocketIDs[msg.ID] != null) {
+// 		if (DATE.getTime() < disabledSocketIDs[msg.ID]) {
+// 			io.to(socket.id).emit('set GCMregID', "Failed to authenticate GCM regID change. You've been locked out.");
+// 			return;
+// 		}
+// 		else {
+// 			disabledSocketIDs[msg.ID] = null;
+// 		}
+// 	}
+// 	if (userSessions[msg.ID] != null && userSessions[msg.ID].authKey == msg.authKey) {
+// 		db.users.find({_id: db.ObjectId(msg.ID)}, function(err, users) {
+// 			db.users.update({_id: db.ObjectId(msg.ID)}, {$set: {GCMregID: msg.GCMregID}});
+// 			io.emit('set GCMregID', "Successfully changed GCM regID.");
+// 			failedAuthAttempts[msg.ID] = null;
+// 		});
+// 	} else {
+// 		io.to(socket.id).emit('set GCMregID', "Failed to authenticate GCM regID change.");
+// 	}
+// });
+// socket.on('set APNSregID', function(msg){
+// 	console.log("set APNS regID");
+// 	stopBruteForce(msg.ID);
+// 	if (disabledSocketIDs[msg.ID] != null) {
+// 		if (DATE.getTime() < disabledSocketIDs[msg.ID]) {
+// 			io.to(socket.id).emit('set APNSregID', "Failed to authenticate APNS regID change. You've been locked out.");
+// 			return;
+// 		}
+// 		else {
+// 			disabledSocketIDs[msg.ID] = null;
+// 		}
+// 	}
+// 	if (userSessions[msg.ID] != null && userSessions[msg.ID].authKey == msg.authKey) {
+// 		db.users.find({_id: db.ObjectId(msg.ID)}, function(err, users) {
+// 			db.users.update({_id: db.ObjectId(msg.ID)}, {$set: {APNSregID: msg.APNSregID}});
+// 			io.emit('set APNSregID', "Successfully changed APNS regID.");
+// 			failedAuthAttempts[msg.ID] = null;
+// 		});
+// 	} else {
+// 		io.to(socket.id).emit('set APNSregID', "Failed to authenticate APNS regID change.");
+// 	}
+// });
+// socket.on('new flyer', function(msg){
+// 	// TODO check to ensure all values are valid (including that they are the correct type)
+// 	console.log("new flyer");
+// 	stopBruteForce(msg.bidder)
+// 	if (disabledSocketIDs[msg.bidder] != null) {
+// 		if (DATE.getTime() < disabledSocketIDs[msg.bidder]) {
+// 			io.to(socket.id).emit('new flyer', "You've failed too many times.");
+// 			return;
+// 		}
+// 		else {
+// 			disabledSocketIDs[msg.bidder] = null;
+// 		}
+// 	}
+// 	// verify user session
+// 	if (userSessions[msg.bidder] != null && userSessions[msg.bidder].authKey == msg.authKey){
+// 		// this still needs lots of work!
+// 		db.items.find({_id: db.ObjectId(msg._id)}, function(err, items) {
+// 			// send flyer notification
+// 			if (items[0].bidHistory[items[0].bidHistory.length - 1].bidder != msg.bidder) {
+// 				// TODO check the othersocket
+// 				if (userSessions[items[0].bidHistory[items[0].bidHistory.length - 1].bidder] != null && connectedSockets[userSessions[items[0].bidHistory[items[0].bidHistory.length - 1].bidder].socketID] == true) {
+// 					console.log("sending a flyer notification");
+// 					io.to(userSessions[items[0].bidHistory[items[0].bidHistory.length - 1].bidder].socketID).emit('outbid notification', {item: msg._id, price: msg.bid, bidder: msg.bidder});
+// 				} else {
+// 					// TODO send a push notification to the user's phone or an email or text
+// 					db.users.find({_id: db.ObjectId(items[0].bidHistory[items[0].bidHistory.length - 1].bidder)}, function(err, users) {
+// 						var GCMregID = "";
+// 						if( err || !users) console.log(err);
+// 						else GCMregID = users[0].GCMregID;
+// 						var message = {
+// 							registration_id: GCMregID, // required
+// 							collapse_key: "New flyers",
+// 							data: {
+// 								item: msg._id,
+// 								price: msg.bid,
+// 								bidder: msg.bidder
+// 							}
+// 						};
+// 						gcm.send(message, function(err, messageId){
+// 							if (err) {
+// 								console.log("Failed to send GCM");
+// 							} else {
+// 								console.log("Sent GCM with ID ", messageId);
+// 							}
+// 						});
+// 					});
+// 					db.users.find({_id: db.ObjectId(items[0].bidHistory[items[0].bidHistory.length - 1].bidder)}, function(err, users) {
+// 						var APNSregID = "";
+// 						if( err || !users) console.log(err);
+// 						else APNSregID = users[0].APNSregID;
+// 						// TODO send an Apple push notification
+// 					});
+// 				}
+// 			}
+// 			db.items.update({_id: db.ObjectId(msg._id)}, {$push: {bidHistory: {bid: msg.bid, bidder: msg.bidder, time: DATE.getHours() + ":" + DATE.getMinutes()}}});
+// 			io.emit('new flyer', {itemID: msg.itemID, _id: msg._id, bid: msg.bid, bidder: msg.bidder, time: DATE.getHours() + ":" + DATE.getMinutes()});
+// 			failedAuthAttempts[msg.bidder] = null;
+// 		});
+// 	} else
+// 		io.to(socket.id).emit('new flyer', "failed to authenticate flyer");
+// });
 
 function authenticateIDauthKey (ID, authKey) {
 	stopBruteForce(ID);
@@ -334,6 +331,6 @@ function comparePassword (correct, testing) {
 
 
 
-server.listen(PORT, function(){
+app.listen(PORT, function(){
 	console.log('listening on *:' + PORT);
 });
